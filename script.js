f3ee7dccb578e2a9ff02c653bc910c0aa68064ec
Gabrielle Lowe <97476695+lowe0142@users.noreply.github.com>
2023-03-26 19:55:43 -0400

    adds array for image gallery

--- script.js
+++ script.js
@@ -1,50 +1,51 @@
+//Gallery Image Array// 
 let images =  [
     {
-        url: "iamge url",
+        url: "images/img1.jpg",
         description: "Image 1"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
+        url: "images/img2.jpg",
+        description: "Image 2"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
+        url: "images/img3.jpg",
+        description: "Image 3"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
+        url: "images/img4.jpg",
+        description: "Image 4"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
+        url: "images/img5.jpg",
+        description: "Image 5"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
+        url: "images/img6.jpg",
+        description: "Image 6"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
+        url: "images/img7.jpg",
+        description: "Image 7"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
+        url: "images/img8.jpg",
+        description: "Image 8"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
+        url: "images/img9.jpg",
+        description: "Image 9"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
+        url: "images/img10.jpg",
+        description: "Image 10"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
+        url: "images/img11.jpg",
+        description: "Image 11"
     },
     {
-        url: "iamge url",
-        description: "Image 1"
-    },
-]
+        url: "images/img12.jpg",
+        description: "Image 12"
+    }
+];
